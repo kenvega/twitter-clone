eb9c25292eb2a4551ebd95cd42d6b8289da699b6
Ken Vega <ken.vega.g@gmail.com>
2023-05-16 21:02:51 -0500

    main add flash messages and update the specs

--- spec/requests/usernames_spec.rb
+++ spec/requests/usernames_spec.rb
@@ -25,16 +25,32 @@ RSpec.describe "Usernames", type: :request do
     context "with a logged in user" do
       before { sign_in user }
 
-      it "updates the username and redirects to the dashboard" do
-        expect do
-          put username_path(user), params: {
-            user: {
-              username: "foobar"
+      context "and a valid username param" do
+        it "updates the username and redirects to the dashboard" do
+          expect do
+            put username_path(user), params: {
+              user: {
+                username: "foobar"
+              }
             }
-          }
-        end.to change { user.reload.username }.from(nil).to("foobar")
+          end.to change { user.reload.username }.from(nil).to("foobar")
 
-        expect(response).to redirect_to(dashboard_path)
+          expect(response).to redirect_to(dashboard_path)
+        end
+      end
+
+      context "and an invalid username param" do
+        it "updates the username and redirects to the dashboard" do
+          expect do
+            put username_path(user), params: {
+              user: {
+                username: ""
+              }
+            }
+          end.not_to change { user.reload.username }
+
+          expect(response).to redirect_to(new_username_path)
+        end
       end
     end
   end
