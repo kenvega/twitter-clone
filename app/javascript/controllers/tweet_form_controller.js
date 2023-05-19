122d0dba00390690a787a0eb702c0842bca06561
Ken Vega <ken.vega.g@gmail.com>
2023-05-19 01:44:10 -0500

    main change tweet form to use turbo stream

---
+++ app/javascript/controllers/tweet_form_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="tweet-form"
+export default class extends Controller {
+  connect() {
+    this.element.addEventListener('turbo:submit-end', () => {
+      // close the modal and clear the form for new tweets
+      document.getElementById('close-modal-btn').click();
+      this.element.reset();
+    })
+  }
+}
