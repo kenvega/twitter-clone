12d72e37d4b0d817d66f92547f1c6dd080f7aad1
Ken Vega <ken.vega.g@gmail.com>
2023-05-31 23:21:59 -0500

    main - add comment to unique index to likes migration to clarify on why it was created

--- db/migrate/20230531232433_add_uniqueness_constraint_to_likes.rb
+++ db/migrate/20230531232433_add_uniqueness_constraint_to_likes.rb
@@ -1,5 +1,6 @@
 class AddUniquenessConstraintToLikes < ActiveRecord::Migration[7.0]
   def change
+    # a unique index is needed because the same user cannot like the same tweet more than once
     add_index :likes, [:user_id, :tweet_id], unique: true
   end
 end
