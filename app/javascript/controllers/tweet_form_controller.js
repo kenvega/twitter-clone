0fca361879a305c3d48bfba6d0d498670e2d0d58
Ken Vega <ken.vega.g@gmail.com>
2023-06-14 11:54:13 -0500

    main - change the modal from full page reload to use turbo stream to update profile info

--- app/javascript/controllers/tweet_form_controller.js
+++ app/javascript/controllers/tweet_form_controller.js
@@ -5,7 +5,10 @@ export default class extends Controller {
   connect() {
     this.element.addEventListener('turbo:submit-end', () => {
       // close the modal and clear the form for new tweets
-      document.getElementById('close-modal-btn').click();
+      // document.getElementById('close-modal-btn').click();
+
+      // click close on all modals with that class
+      Array.from(document.getElementsByClassName('close-modal-btn')).forEach((btn) => { btn.click() })
       this.element.reset();
     })
   }
