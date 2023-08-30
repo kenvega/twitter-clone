b4a7cf7aaf5cd9e82431b4cddfc3d96eb63b937f
Ken Vega <ken.vega.g@gmail.com>
2023-08-30 16:31:53 -0500

    main - add counters for followers and followed_users and change url to profile_url for User model

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,6 @@
 class Follow < ApplicationRecord
-  belongs_to :follower, class_name: "User" # if this does not have the class_name option the following error appears => NameError: Rails couldn't find a valid model for Follower association
-  belongs_to :followed, class_name: "User"
+  belongs_to :follower, class_name: "User", counter_cache: :followed_users_count # if this does not have the class_name option the following error appears => NameError: Rails couldn't find a valid model for Follower association
+  belongs_to :followed, class_name: "User", counter_cache: :follower_count
 
   validates :follower_id, uniqueness: { scope: :followed_id }
 end
