21e3f46ce7357d732f1608ed959983ab8bfac1f4
Ken Vega <ken.vega.g@gmail.com>
2023-09-10 11:21:04 -0500

    main - update avatar photo on profile view

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,16 +1,17 @@
 class ProfileController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_current_profile_user
 
   def show
-    @current_profile_user = current_user
-    @tweet_presenters = @current_profile_user.tweets.map do |tweet|
-      TweetPresenter.new(tweet: tweet, current_user: @current_profile_user)
-    end
+    set_tweet_presenters
     render "users/show"
   end
 
   def update
-    current_user.update(user_params[:password].blank? ? user_params.except(:password) : user_params)
+    @current_profile_user.update(user_params[:password].blank? ? user_params.except(:password) : user_params)
+
+    set_tweet_presenters
+
     respond_to do |format|
       format.html { redirect_to profile_path }
       format.turbo_stream
@@ -19,7 +20,17 @@ class ProfileController < ApplicationController
 
   private
 
+  def set_current_profile_user
+    @current_profile_user = current_user
+  end
+
+  def set_tweet_presenters
+    @tweet_presenters = @current_profile_user.tweets.map do |tweet|
+      TweetPresenter.new(tweet: tweet, current_user: @current_profile_user)
+    end
+  end
+
   def user_params
-    params.require(:user).permit(:username, :display_name, :email, :password, :bio, :location, :profile_url)
+    params.require(:user).permit(:username, :display_name, :email, :password, :bio, :location, :avatar, :profile_url)
   end
 end
