acbc0abedbfca7638a1a1b2355f9ba0eb652c009
Ken Vega <ken.vega.g@gmail.com>
2023-11-04 18:37:21 -0500

    main render channels in the messages page. for now we have a bug where two channels can appear. one of the already created one and another for the builded one.

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,6 +2,11 @@ class ChannelsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # for the conversations that the current user already have
+    current_user_subscribed_channel_ids = Subscription.where(user: current_user).pluck(:channel_id)
+    @current_user_subscribed_channels = Channel.where(id: current_user_subscribed_channel_ids)
+
+    # for the new conversation the user is about to begin
     if params[:user_id].present?
       @new_subscription = User.find(params[:user_id])
     end
