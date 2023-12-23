228c553a11e802ed80117428e6c975700f84b4a4
Ken Vega <ken.vega.g@gmail.com>
2023-12-23 11:05:27 -0500

    main - rendering followed me notifications on notifications index page

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,5 +5,10 @@ class Notification < ApplicationRecord
 
   ACTIONS = %w[followed-me liked-tweet mentioned-me].freeze
 
+  # meta programming to create methods like followed_me?, liked_tweet?, etc
+  ACTIONS.each do |a|
+    define_method("#{a.gsub('-', '_')}?") { a == action }
+  end
+
   validates :action, presence: true, inclusion: { in: ACTIONS }
 end
