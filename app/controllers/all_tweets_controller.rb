bed5833620cf30fad7be0088ec9a82d317713e76
Ken Vega <ken.vega.g@gmail.com>
2024-03-18 01:48:25 -0500

    main - order all tweets by created at desc

--- app/controllers/all_tweets_controller.rb
+++ app/controllers/all_tweets_controller.rb
@@ -2,7 +2,7 @@ class AllTweetsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @all_tweets = Tweet.all.includes(user: { avatar_attachment: :blob }).map do |tweet|
+    @all_tweets = Tweet.all.includes(user: { avatar_attachment: :blob }).order(created_at: :desc).map do |tweet|
       TweetPresenter.new(tweet: tweet, current_user: current_user)
     end
   end
